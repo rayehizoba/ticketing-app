73d56b0492032b1f8d9c2ef5d5ecf2364d316f37
Raymond Ehizoba <raymondehizoba@Raymonds-MacBook-Pro.local>
2018-06-18 12:47:01 +0100

    Creating custom side drawer component

--- App.js
+++ App.js
@@ -1,29 +1,64 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
-import RegisterScreen from './screens/RegisterScreen';
-import HomeScreen from './screens/HomeScreen';
-import { Font } from 'expo';
+import {
+  SafeAreaView, Text, View, Button,
+  Dimensions, Animated, Easing, TouchableWithoutFeedback
+} from 'react-native';
 
-const RootStack = createStackNavigator({
-  Register: {
-    screen: RegisterScreen
-  },
-  Home: {
-    screen: HomeScreen
-  },
-}, {
-  headerMode: 'none',
-});
+const {width, height} = Dimensions.get('window');
 
 export default class App extends React.Component {
-  state = {fontLoaded: false};
-  async componentDidMount() {
-    await Font.loadAsync({
-      'OpenSansRegular': require('./assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
+  state = {
+    isDrawerOpen: false,
+  };
+  constructor(props) {
+    super(props);
+    this.animation = new Animated.Value(0);
+  }
+  openDrawer() {
+    Animated.timing(this.animation, {
+      toValue: 1,
+      duration: 800,
+      easing: Easing.out(Easing.exp)
+    }).start(() => {
+      this.setState({isDrawerOpen: true});
     });
-    this.setState({fontLoaded: true});
+  }
+  closeDrawer() {
+    Animated.timing(this.animation, {
+      toValue: 0,
+      duration: 800,
+      easing: Easing.out(Easing.exp)
+    }).start(() => {
+      this.setState({isDrawerOpen: false});
+    });
   }
   render() {
-    return this.state.fontLoaded ? <RootStack /> : null;
+    const overlayOpacity = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 0.5]
+    });
+    const drawerTranslate = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-width, 0]
+    });
+    return (
+      <SafeAreaView style={{flex: 1}} >
+        <Text>Main Content</Text>
+        <Button title="Open side drawer" onPress={() => this.openDrawer()} />
+        {/* background overlay */}
+        <Animated.View pointerEvents={this.state.isDrawerOpen ? 'auto' : 'none'}
+          style={{backgroundColor: 'black', opacity: overlayOpacity,
+          flex: 1, position: 'absolute', width, height}} >
+          <TouchableWithoutFeedback onPress={() => this.closeDrawer()}>
+            <View style={{flex: 1}} />
+          </TouchableWithoutFeedback>
+        </Animated.View>
+        <Animated.View style={{backgroundColor: 'white', opacity: 1, flex: 1,
+          position: 'absolute', width: ((80/100)*width), height,
+          marginRight: ((20/100)*width),
+          transform: [{translateX: 0}], shadowColor: 'black',}} >
+        </Animated.View>
+      </SafeAreaView>
+    );
   }
 }
